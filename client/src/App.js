2d6c06284b7cac13c9a9633f84266fc541c46bf8
chen-ryan1337 <ryanchen1337@gmail.com>
2024-11-28 17:35:58 -0500

    Added error labelling and fixed naming issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homePage";
 import LoginPage from "./pages/loginPage";
+import RegisterPage from "./pages/registerPage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/pages/loginPage.jsx
+++ client/src/pages/loginPage.jsx
@@ -5,30 +5,49 @@ import './pages.css'
 const LoginPage = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [loginError, setLoginError] = useState('');
   const navigate = useNavigate();
 
   const handleSubmit = async(e) => {
+    e.preventDefault(); // Prevent form from submitting and reloading the page
+
+    // checks if email is valid
+    if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)) {
+      setEmailError('Please enter a valid email')
+      return;
+    }
+
     // Send login info to the server for verification
-    //...
+    
+    // .. if response.status is 401 it means unauthorized because the credentials don't exists
+      //... if 401, then update loginError = 'invalid email or password' 
+
+    // .. otherwise, proceed by assingning user access token and redirecting to home page
+
+      // navigate("./Home");
+
+  
   };
 
   return (
     <div className="mainContainer">
       <div className="forumContainer">
         <div className="content">
-        <div className="titleContainer">Login</div>
+        <div className="titleContainer">Login <label className="errorLabel">{loginError}</label></div>
         <form onSubmit={handleSubmit} className="inputContainer">
             <div>
               <label>Email</label>
-              <div>
-                <input className="inputBox" name="username" type="text" required />
+              <div style={{display: "flex", flexDirection: "column"}}>
+                <input className="inputBox" name="username" type="text" required onChange={(e) => setEmail(e.target.value)}/>
+                <label className="errorLabel">{emailError}</label>
               </div>
             </div>
 
-            <div>
+            <div style={{display: "flex", flexDirection: "column"}}>
               <label>Password</label>
               <div>
-                <input className="inputBox" name="username" type="password" required />
+                <input className="inputBox" name="password" type="password" required onChange={(e) => setPassword(e.target.value)}/>
               </div>
             </div>
 
