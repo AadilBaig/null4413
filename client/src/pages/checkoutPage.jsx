9d7d60e785f2a9e6163a4b524013d82e7c6a32aa
chen-ryan1337 <ryanchen1337@gmail.com>
2024-12-19 18:10:17 -0500

    created checkout front end

--- client/src/pages/checkoutPage.jsx
+++ client/src/pages/checkoutPage.jsx
@@ -1,8 +1,20 @@
-import React, {useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import { useNavigate, useParams} from 'react-router-dom'
 import { useCookie } from '../global/CookieContext'
+import './pages.css'
 
 const CheckoutPage = () => {
+
+    const { id } = useParams();
+
+    // address info
+    const [streetName, setStreetName] = useState("");
+    const [province, setProvince] = useState("");
+    const [country, setCountry] = useState("");
+    const [zip, setZip] = useState("");
+    const [phone, setPhone] = useState("");
+    const [cardNum, setCardNum] = useState("");
+
 
     // Get cookie methods from our context api class "CookieContext"
     const { cookieData, saveCookieData, clearCookieData, appendToCart, updateItemQtyInCart } = useCookie();
@@ -16,12 +28,69 @@ const CheckoutPage = () => {
             navigate("/");
         }
         else if (cookieData.role === 'guest'){
-            alert("You do not have permission to proceed to this page");
+            alert("You must sign in to proceed to this page");
             navigate("/");
         }
+
+        // if user is permitted then fetch user's address (including their credit care number)
+        const fetchUserAddress = async() => {
+
+        }
+
+        // invoke
+        fetchUserAddress();
     }, [cookieData])
   return (
-    <div>checkoutPage</div>
+    <>
+        <a href="/cart" style={{position: "absolute", padding: "3rem"}}>Back To Cart</a>
+        <div className="mainContainer">
+            <div className="fillCheckOutContainer">
+                <h1 style={{textAlign: "center"}}>Checkout</h1>
+            <form className="inputContainer"> 
+                <div>
+                    <label>Street Name</label>
+                    <div>
+                    <input className="inputBox" name="streetName" type="text" required onChange={(e) => setStreetName(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Province</label>
+                    <div>
+                    <input className="inputBox" name="province" type="text" required onChange={(e) => setProvince(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Country</label>
+                    <div>
+                    <input className="inputBox" name="country" type="text" required onChange={(e) => setCountry(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Zip</label>
+                    <div>
+                    <input className="inputBox" name="zip" type="text" required onChange={(e) => setZip(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Phone Number</label>
+                    <div>
+                    <input className="inputBox" name="phoneNumber" type="text" required onChange={(e) => setPhone(e.target.value)}/>
+                    </div>
+                </div>
+                <div>
+                    <label>Credit Card Number</label>
+                    <div>
+                    <input className="inputBox" name="creditCard" type="text" required onChange={(e) => setCardNum(e.target.value)}/>
+                    </div>
+                </div>
+                
+                    <div style={{width: '225px', margin: "1rem"}}>
+                    <button className="submitButton" type="submit">Submit</button>
+                    </div>
+            </form>
+            </div>
+        </div>
+    </>
   )
 }
 
